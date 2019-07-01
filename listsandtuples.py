f960642295b07d36a1eec6f840baff8fe204de4f
Carolyn <52383366+cgpitta@users.noreply.github.com>
2019-06-30 22:34:08 -0400

    Add files via upload

---
+++ listsandtuples.py
@@ -0,0 +1,14 @@
+#write a program which accepts a sequence of comma-separated numbers fomr console 
+#and generate a list and a tuple which contain every number
+
+
+val1=input('Enter Values: ')
+
+li1=val1.split() 
+
+tup=tuple(li1)
+
+
+print(li1)
+
+print(tup)
